5a19564c3f3ab6c616a9b7509476e608e0326e4e
Friedrich Hanisch <friedrich@ratking.de>
2025-02-18 11:54:35 +0100

    move gimpl to subfolder

--- Project/test.gd
+++ Project/test.gd
@@ -7,20 +7,26 @@ func func_call_1():
 
 func func_call_2(a, b, c = "hallo"):
 	printt(a, b, c)
+	return "return!"
 
 func _ready() -> void:
 	var gimpl := Gimpl.new(self)
 	
-	gimpl.eval("func_call_1(); func_call_2(1, 2)")
+	gimpl.eval('
+		func_call_1();
+		func_call_2("foo", 2)')
 	
-	gimpl.eval("n = 5;
+	gimpl.eval("
+		n = 5;
 		p = 1;
 		while n > 0 do
 		  p = p * n;
 		  n = n - 1
 		end")
 	
-	gimpl.eval('test = "str";
+	gimpl.eval('
+		test = "str";
+		x = x + 5;
 		t = if test != "str" then 100 else 50 end;
 		r = s = 5;
 		w = false;
