6779f4853b55306c09c0c7e6d0fe9b919d7583fb
Friedrich Hanisch <friedrich@ratking.de>
2026-08-17 22:04:55 +0200

    improving live coding scene/code

--- live.gd
+++ live.gd
@@ -1,11 +1,13 @@
 extends Sprite2D
 
-@export_multiline var code := "speed = array(-2, 5, 20).pick_random()\nwhile true do\n\tdt = get_process_delta_time()\n\trotate(speed * dt)\n\t// move_local_x(5)\n\tinterrupt\nend"
+@export_multiline var code := "counter = 0\nspeed = array(-2, 5, 20).pick_random()\nwhile true do\n\tdt = get_process_delta_time()\n\trotate(speed * dt)\n\t// move_local_x(5)\n\tprint(counter = counter + 1)\n\tinterrupt\nend"
 @export var code_editor: TextEdit
 @export var code_compile: Button
 @export var code_error: Label
+@export var maxsteps_edit: TextEdit
 
 var state := {}
+var maxsteps := 2000
 
 @onready var orig_transform := self.transform
 @onready var gompl := Gompl.new(self)
@@ -13,19 +15,23 @@ var state := {}
 ###
 
 func _ready() -> void:
-	gompl.register_func("print", func(a): print(a); code_error.text = str(a), [ Gompl.T_ANY ])
+	gompl.register_func("print", func(a): code_error.text += str(a, "\n"), [ Gompl.T_ANY ])
 	gompl.register_func("rnd", func(): return randf())
 	gompl.register_func("mouse_pos", func() -> Vector2: return get_viewport().get_mouse_position())
 	gompl.register_func("v2", func(x: float, y: float) -> Vector2: return Vector2(x, y), [ Gompl.T_NUMBER, Gompl.T_NUMBER ])
 	
 	code_editor.text = code
 	code_compile.button_down.connect(on_code_compile)
+	maxsteps_edit.text_changed.connect(on_maxsteps_edit)
+	maxsteps_edit.text = str(maxsteps)
 	on_code_compile()
 
 func _process(_delta: float) -> void:
+	
+	code_error.text = ""
 	if not gompl.err:
 		# the code is compiled every frame, this is wasteful, but okay for this small example
-		gompl.eval(code, null, state, 5000)
+		gompl.eval(code, null, state, maxsteps)
 		if gompl.err: code_error.text = gompl.err
 		#gompl.debug_printing = false
 
@@ -38,3 +44,7 @@ func on_code_compile() -> void:
 	code = code_editor.text
 	gompl.err = ""
 	transform = orig_transform
+
+func on_maxsteps_edit() -> void:
+	maxsteps = clampi(int(maxsteps_edit.text), 1, 5000)
+	maxsteps_edit.text = str(maxsteps)
